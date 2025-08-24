0791b1e0ad1f3f8d88efefd425fd9488ff99a383
Josue-JP <josueportillod07@gmail.com>
2025-08-24 15:04:23 -0400

    Update ser.py

--- ser.py
+++ ser.py
@@ -5,70 +5,79 @@ import sys
 HOST = ''
 PORT = 12345
 
-times_to_repeat = 0
+def checkname(cli_file_name, directory):
+    base, ext = os.path.splitext(cli_file_name)
+    filename = cli_file_name
+    counter = 1
 
-def check(text):
-    user_input = input(text)
-    if user_input.lower() == "q":
-        print("Quitting")
-        sys.exit()
-    return user_input
+    while os.path.exists(os.path.join(directory, filename)):
+        filename = f"{base}_{counter}{ext}"
+        counter += 1
 
-def receive_file(connection):
-    global times_to_repeat
-    try:
-        repeat_buffer = b""
-        while b"\n" not in repeat_buffer:
-            repeat_buffer += connection.recv(1)
-        repeat = int(repeat_buffer.decode().strip())
-        times_to_repeat += repeat
+    return os.path.join(directory, filename)
 
-        # get the file SIZE
-        size_buffer = b""
-        while b"\n" not in size_buffer:
-            size_buffer += connection.recv(1)
-        filesize = int(size_buffer.decode().strip())
-
-        cli_file_name_buffer = b""
-        while b"\n" not in cli_file_name_buffer:
-            cli_file_name_buffer += connection.recv(1)
-        cli_file_name = cli_file_name_buffer.decode().strip()
-
-        file_location = check(f"Enter the path to save {cli_file_name}: ")
+def receive_file(connection, directory):
+    while True:
+        try:
+            # get the file SIZE
 
 
-        received = 0
 
-        with open(file_location, "wb") as f:
-            while received < filesize:
-                chunk = connection.recv(min(1024, filesize - received))
-                if not chunk: break
+            size_buffer = b""
+            while b"\n" not in size_buffer:
+                size_buffer += connection.recv(1)
+            filesize = int(size_buffer.decode().strip())
 
-                f.write(chunk)
-                received += len(chunk)
+            cli_file_name_buffer = b""
+            while b"\n" not in cli_file_name_buffer:
+                cli_file_name_buffer += connection.recv(1)
+            cli_file_name = cli_file_name_buffer.decode().strip()
+            cli_file_name = os.path.basename(cli_file_name)
 
 
-        print(f"Saved {cli_file_name} as {file_location}")
-    except Exception as e:
-        print(f"ERROR: {e}")
+            file_location = checkname(cli_file_name, directory)
+
+
+            received = 0
+
+            with open(file_location, "wb") as f:
+                while received < filesize:
+                    chunk = connection.recv(min(1024, filesize - received))
+                    if not chunk: break
+
+                    f.write(chunk)
+                    received += len(chunk)
+
+
+            print(f"Saved {cli_file_name} as {file_location}")
+        except KeyboardInterrupt:
+            print("\nProgram ended by user")
+            break
+        except Exception as e:
+            print(f"ERROR: {e}")
 
 def sock():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.bind((HOST, PORT))
         sock.listen(1)
-        sock.settimeout(3)
-        while True:
-            try:
-                connection, address = sock.accept()
-                with connection:
-                    receive_file(connection)
-            except socket.timeout:
-                continue
-            except KeyboardInterrupt:
-                print("\nProgram ended by user")
-                break
-            except Exception as e:
-                print(f"Server Error: {e}")
+        sock.settimeout(120)
+        try:
+            connection, address = sock.accept()
+            with connection:
+                confirmation_buffer = b""
+                while b"\t" not in confirmation_buffer:
+                    confirmation_buffer += connection.recv(1)
+                connection.sendall(confirmation_buffer)
+
+                directory = input("Specify the directory to save each file: ")
+                os.makedirs(directory, exist_ok=True)
+
+                receive_file(connection, directory)
+
+        except socket.timeout:
+            print("socket timeout")
+        except Exception as e:
+            print(f"Server Error: {e}")
 
 def main():
     try:
@@ -83,3 +92,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
